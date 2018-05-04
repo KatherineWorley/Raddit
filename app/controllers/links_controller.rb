8d931a67cbdf218c1ab31ad54272ca2e5794e416
Katherine Worley <kdesign216@yahoo.com>
2018-05-04 11:41:21 -0400

    made assocation between links and users

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,6 +1,6 @@
 class LinksController < ApplicationController
   before_action :set_link, only: [:show, :edit, :update, :destroy]
-  before_filter :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show]
 
   # GET /links
   # GET /links.json
