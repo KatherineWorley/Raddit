542d0c6e96887697a701b6462c05d680f637d16a
Katherine Worley <kdesign216@yahoo.com>
2018-05-14 10:57:47 -0400

    updating comments model

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -24,14 +24,16 @@ class CommentsController < ApplicationController
   # POST /comments
   # POST /comments.json
   def create
-    @comment = Comment.new(comment_params)
+    @link = Link.find(params[:link_id])
+    @comment = @link.comment.new(comment_params)
+    @comment.user = current_user
 
     respond_to do |format|
       if @comment.save
         format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
         format.json { render :show, status: :created, location: @comment }
       else
-        format.html { render :new }
+        format.html { render action: "new" }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   		put "like", to: "links#upvote"
   		put "dislike", to: "links@downvote"
   	end 
+
+    resources :comments
   end
   
   root to: "links#index"
