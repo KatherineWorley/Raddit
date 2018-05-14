e409635e2d5dd08147fe7c9329d420f2d5300833
Katherine Worley <kdesign216@yahoo.com>
2018-05-14 08:13:11 -0400

    working on acts as votable gem

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,6 +1,7 @@
 class LinksController < ApplicationController
   before_action :set_link, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :upvote, :downvote, except: :index
 
   # GET /links
   # GET /links.json
