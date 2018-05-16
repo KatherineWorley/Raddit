f395aeb7618098fbba8abaceede7dcff93c35745
Katherine Worley <kdesign216@yahoo.com>
2018-05-16 06:48:34 -0400

    updating links controler and creating comment view

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -2,6 +2,7 @@ class LinksController < ApplicationController
   before_action :set_link, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
   before_action :upvote, :downvote, except: :index
+  before_action :authorized_user, only: [:edit, :update, :destroy]
 
   # GET /links
   # GET /links.json
@@ -66,7 +67,7 @@ class LinksController < ApplicationController
   def upvote
     @link = Link.find(params[:id])
     @link.upvote_by current_user
-    redirect_back fallback_location: root_path
+    redirect_back
   end
 
   def downvote
