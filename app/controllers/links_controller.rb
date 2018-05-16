df611e9d9d223efbdb168175bcbfd77898aa03fd
Katherine Worley <kdesign216@yahoo.com>
2018-05-16 07:48:23 -0400

    fixed before_action and links show controller

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,7 +1,6 @@
 class LinksController < ApplicationController
   before_action :set_link, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :upvote, :downvote, except: :index
   before_action :authorized_user, only: [:edit, :update, :destroy]
 
   # GET /links
