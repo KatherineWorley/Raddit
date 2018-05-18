ffde07404458011664cb5999b7f04fa4f7da53eb
Katherine Worley <kdesign216@yahoo.com>
2018-05-18 07:47:51 -0400

    updating links controller

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,7 +1,7 @@
 class LinksController < ApplicationController
   before_action :set_link, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :authorized_user, only: [:show, :create, :edit, :update, :destroy]
+  before_action :authorized_user, only: [:edit, :update, :destroy]
 
   # GET /links
   # GET /links.json
